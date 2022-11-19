d16390e417a46dadeb25061026e3354ac91ddc12
Vitvita222 <115868179+Vitvita222@users.noreply.github.com>
2022-11-19 14:14:14 +0400

    Add files via upload

--- vito.js
+++ vito.js
@@ -1,20 +1,27 @@
+//inputs
 const input1 = document.querySelector('.input1')
-const btn1 = document.querySelector('.submit')
-
 const input2 = document.querySelector('.input2')
+const input3 = document.querySelector('.input3')
+const input4 = document.querySelector('.input4')
+
+
+
+// buttons
+const btn1 = document.querySelector('.submit')
 const btn2 = document.querySelector('.check')
 
 var data = []
 
 btn1.addEventListener('click', () =>{
     data.push(input1.value)
+    data.push(input2.value)
 })
 
 
 btn2.addEventListener('click', () => {
-    if (input2.value == data[0] || input2.value == data[1] || input2.value == data[2] || input2.value == data[3]|| input2.value == data[4] || input2.value == data[5] ||input2.value == data[6] ||input2.value == data[2] ||input2.value == data[7] ||input2.value == data[8] ||input2.value == data[9] ||input2.value == data[10] ||input2.value == data[11] ||input2.value == data[12] ||input2.value == data[13] ||input2.value == data[14] ||input2.value == data[15] ||input2.value == data[16] ||input2.value == data[17] ||input2.value == data[18] ||input2.value == data[19] ||input2.value == data[20] ||input2.value == data[21] ||input2.value == data[22] ||input2.value == data[23] ||input2.value == data[23] ||input2.value == data[24] ||input2.value == data[25] ||input2.value == data[26] ||input2.value == data[27] ||input2.value == data[28] ||input2.value == data[29] || input2.value == data[30]) {
-        document.body.style.backgroundColor = 'green'
+    if (input3.value == data[0] && input4.value == data[1] || input3.value == data[2] && input4.value == data[3] || input3.value == data[4] && input4.value == data[5] || input3.value == data[6] && input4.value == data[7] || input3.value == data[8] && input4.value == data[9] ||input3.value == data[10] && input4.value == data[11] ||input3.value == data[12] && input4.value == data[13] ||input3.value == data[14] && input4.value == data[15] ||input3.value == data[16] && input4.value == data[17] ||input3.value == data[18] && input4.value == data[19] ||input3.value == data[20] && input4.value == data[21] ||input3.value == data[22] && input4.value == data[23] ||input3.value == data[24] && input4.value == data[25] ||input3.value == data[26] && input4.value == data[27] ||input3.value == data[28] && input4.value == data[29] ||input3.value == data[30] && input4.value == data[31]){
+        alert("Logged In")
     } else {
-        document.body.style.backgroundColor = 'red'
+        alert("This Account Doesn't Exist")
     }
 })
